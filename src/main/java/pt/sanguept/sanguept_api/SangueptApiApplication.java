3968df3975ed8779e7b64774d25289ac7ec9a7f0
Ratmir Mukazhanov <ratmir.mukazhanov@devlop.systems>
2026-06-07 15:13:09 +0100

    init project

---
+++ src/main/java/pt/sanguept/sanguept_api/SangueptApiApplication.java
@@ -0,0 +1,13 @@
+package pt.sanguept.sanguept_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangueptApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangueptApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/sanguept/sanguept_api/SangueptApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.sanguept.sanguept_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangueptApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
